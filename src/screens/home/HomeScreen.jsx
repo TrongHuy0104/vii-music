a4a379e523ba8087f7add411b52522a1931d6b8b
Thanh Tuyền <141143887+tuyenhttt@users.noreply.github.com>
2024-10-05 22:23:37 +0700

    Update HomeScreen.jsx

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -3,13 +3,13 @@ import { StyleSheet, View } from 'react-native'
 import Header from '../../components/Header'
 import { defaultStyles } from '../../styles'
 
-import NewReleaseGrid from './NewRelease'
+import NewRelease from './NewRelease'
 
 export default function HomeScreen() {
 	return (
 		<View style={defaultStyles.container}>
 			<Header title="Trang chủ" />
-			<NewReleaseGrid />
+			<NewRelease />
 		</View>
 	)
 }
